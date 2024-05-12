7b8547c42fd04edf20bc27771a2552d740ffc801
alamin-karno <amin15-1951@diu.edu.bd>
2024-05-12 16:56:09 +0600

    BlogXpress: Initialization

---
+++ src/main/java/com/alaminkarno/blogxpress/BlogXpressApplication.java
@@ -0,0 +1,13 @@
+package com.alaminkarno.blogxpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogXpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogXpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alaminkarno/blogxpress/BlogXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaminkarno.blogxpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

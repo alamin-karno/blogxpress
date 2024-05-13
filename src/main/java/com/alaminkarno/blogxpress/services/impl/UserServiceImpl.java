5ba34aafe782b1c963b3f5e31e1e7a28c1d68a95
alamin-karno <amin15-1951@diu.edu.bd>
2024-05-13 14:27:48 +0600

    BlogXpress: ModelMapper Added

--- src/main/java/com/alaminkarno/blogxpress/services/impl/UserServiceImpl.java
+++ src/main/java/com/alaminkarno/blogxpress/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.alaminkarno.blogxpress.exceptions.ResourceNotFoundException;
 import com.alaminkarno.blogxpress.payloads.UserDto;
 import com.alaminkarno.blogxpress.repositories.UserRepository;
 import com.alaminkarno.blogxpress.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -57,26 +61,10 @@ public class UserServiceImpl implements UserService {
     }
 
     private User dtoToUser(UserDto userDto){
-        User user = new User();
-
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setEmail(userDto.getEmail());
-        user.setPassword(userDto.getPassword());
-        user.setAbout(userDto.getAbout());
-
-        return user;
+        return this.modelMapper.map(userDto,User.class);
     }
 
     private UserDto userToDto(User user){
-        UserDto userDto = new UserDto();
-
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setEmail(user.getEmail());
-        userDto.setPassword(user.getPassword());
-        userDto.setAbout(user.getAbout());
-
-        return userDto;
+        return this.modelMapper.map(user,UserDto.class);
     }
 }
